953c7a2c4687daeb182b39b79681b52ff6fa524e
Brett <bretttucker@gmail.com>
2025-07-13 11:23:15 -0600

    Provide datasource via function and lambda

--- src/main/kotlin/com/bltucker/recipemanager/database/Database.kt
+++ src/main/kotlin/com/bltucker/recipemanager/database/Database.kt
@@ -4,11 +4,27 @@ import com.bltucker.recipemanager.database.tables.Recipes
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.Application
+import io.ktor.server.plugins.di.dependencies
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
-fun Application.configureDatabase() {
+suspend fun Application.configureDatabase() {
+    dependencies{
+        provide { provideDatasource() }
+    }
+
+    val dataSource = dependencies.resolve<HikariDataSource>()
+    Database.connect(dataSource)
+
+    transaction {
+        SchemaUtils.create(Recipes)
+    }
+}
+
+
+private fun provideDatasource(): HikariDataSource{
+    println("Creating Datasource")
     val dbHost = System.getenv("DB_HOST") ?: "localhost"
     val dbPort = System.getenv("DB_PORT") ?: "5432"
     val dbName = System.getenv("DB_NAME") ?: "recipemanager_dev"
@@ -30,9 +46,5 @@ fun Application.configureDatabase() {
         validate()
     })
 
-    Database.connect(dataSource)
-
-    transaction {
-        SchemaUtils.create(Recipes)
-    }
+    return dataSource
 }
